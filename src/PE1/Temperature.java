b2091db82987a875a71f88ef80528a6069ad6aaf
RachelFeng924 <rachel2298277@gmail.com>
2023-12-31 19:24:41 -0800

    new start

--- src/PE1/Temperature.java
+++ src/PE1/Temperature.java
@@ -1,7 +1,17 @@
 package PE1;
 public class Temperature {
     public static void main(String[] args) {
-        System.out.println("hi");
-        //hi
+        float farenheit = 450.0f;
+
+        double celsius = (farenheit - 32)/1.8;
+
+        double kelvin = celsius;
+
+        kelvin += 273.15;
+
+        System.out.println(farenheit);
+        System.out.println(celsius);
+        System.out.println(kelvin);
+
     }
 }
