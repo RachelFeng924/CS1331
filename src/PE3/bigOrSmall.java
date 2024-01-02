3cacbbd4eafd9b028668635b053f2191884fd002
RachelFeng924 <rachel2298277@gmail.com>
2024-01-02 14:03:15 -0800

    finished assignment

--- src/PE3/bigOrSmall.java
+++ src/PE3/bigOrSmall.java
@@ -1,4 +1,20 @@
 package PE3;
 
 public class bigOrSmall {
+    public static void main(String[] args) {
+        int smallRandNum = 5 - (int) (Math.random()*4);
+        System.out.println("We will run " + smallRandNum + " times!");
+
+        int n;
+        for (int i = 0; i < smallRandNum; i++) {
+            n = 20 - (int) (Math.random()*19);
+            System.out.println("The value is: " + n);
+            if (n > 10) {
+                System.out.println("Yay!");
+            }
+            else {
+                System.out.println("Nay!");
+            }
+        }
+    }
 }
